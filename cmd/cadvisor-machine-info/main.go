3728c8e72330be4ed02299027bfc826336960009
Carter McKinnon <cmckinnon@coreweave.com>
2025-10-29 13:45:07 -0700

    Initial commit

---
+++ cmd/cadvisor-machine-info/main.go
@@ -0,0 +1,38 @@
+package cadvisormachineinfo2
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+
+	"k8s.io/kubernetes/pkg/kubelet/cadvisor"
+)
+
+func main() {
+	client, err := cadvisor.New(&unsupportedImageFsInfoProvider{}, "/", []string{}, false /* usingLegacyStats */, false /* localStorageCapacityIsolation */)
+	if err != nil {
+		log.Fatalf("failed to setup cadvisor: %v", err)
+	}
+	info, err := client.MachineInfo()
+	if err != nil {
+		log.Fatalf("failed to get machine info: %v", err)
+	}
+	log.Printf("Machine Info: %+v", info)
+	infoJson, err := json.MarshalIndent(info, "", "  ")
+	if err != nil {
+		log.Fatalf("failed to marshal machine info: %v", err)
+	}
+	fmt.Println(infoJson)
+}
+
+// unsupportedImageFsInfoProvider is a no-op implementation of ImageFsInfoProvider.
+type unsupportedImageFsInfoProvider struct{}
+
+func (i *unsupportedImageFsInfoProvider) ImageFsInfoLabel() (string, error) {
+	return "", errors.New("unsupported")
+}
+
+func (i *unsupportedImageFsInfoProvider) ContainerFsInfoLabel() (string, error) {
+	return "", errors.New("unsupported")
+}
